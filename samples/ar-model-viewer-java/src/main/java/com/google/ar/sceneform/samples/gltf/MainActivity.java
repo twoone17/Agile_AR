0689448fac6da675c8a4e5b2a03a79b90955ee02
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-07-25 00:01:16 +0900

    Initial commit

---
+++ samples/ar-model-viewer-java/src/main/java/com/google/ar/sceneform/samples/gltf/MainActivity.java
@@ -0,0 +1,171 @@
+package com.google.ar.sceneform.samples.gltf;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentOnAttachListener;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Config;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.SceneView;
+import com.google.ar.sceneform.Sceneform;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.rendering.ViewRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity implements
+        FragmentOnAttachListener,
+        BaseArFragment.OnTapArPlaneListener,
+        BaseArFragment.OnSessionConfigurationListener,
+        ArFragment.OnViewCreatedListener {
+
+    private ArFragment arFragment; //
+    private Renderable model;
+    private ViewRenderable viewRenderable;
+
+    /**
+     * 생명주기 1번째)
+     * activity 생성, 3D 모델 load
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().addFragmentOnAttachListener(this);
+
+        if (savedInstanceState == null) {
+            if (Sceneform.isSupported(this)) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.arFragment, ArFragment.class, null)
+                        .commit();
+            }
+        }
+
+        /** 의성
+         * 3D 모델을 load한다
+         */
+        loadModels();
+    }
+
+
+    /**
+     * 생명주기 2번째)
+     * inflate한 frament가 activity에 붙여짐 (attach)
+     */
+    @Override
+    public void onAttachFragment(@NonNull FragmentManager fragmentManager, @NonNull Fragment fragment) {
+        if (fragment.getId() == R.id.arFragment) {
+            arFragment = (ArFragment) fragment;
+            arFragment.setOnSessionConfigurationListener(this);
+            arFragment.setOnViewCreatedListener(this);
+            arFragment.setOnTapArPlaneListener(this);
+        }
+    }
+    /**
+    * 세션안에 들어갈 모드를 모은다, 합친다, 구성정보 설정한다고 생각
+    */
+    @Override
+    public void onSessionConfiguration(Session session, Config config) {
+        if (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
+            config.setDepthMode(Config.DepthMode.AUTOMATIC);
+        }
+    }
+
+    /**
+     * 생명주기 3번째)
+     * frament 안에 view를 만든다
+     */
+    @Override
+    public void onViewCreated(ArSceneView arSceneView) {
+        arFragment.setOnViewCreatedListener(null);
+
+        // Fine adjust the maximum frame rate
+        arSceneView.setFrameRateFactor(SceneView.FrameRate.FULL);
+    }
+
+    /**
+     * plugin을 사용하는 것이 아닌 glb or gltf 파일, 혹은 url을 로드한다
+     */
+    public void loadModels() {
+        //3D 모델 load
+        WeakReference<MainActivity> weakActivity = new WeakReference<>(this);
+        ModelRenderable.builder()
+                //.setSource(this, Uri.parse("https://storage.googleapis.com/ar-answers-in-search-models/static/Tiger/model.glb"))
+                .setSource(this, Uri.parse("models/cube.glb"))
+                .setIsFilamentGltf(true)
+                .setAsyncLoadEnabled(true)
+                .build()
+                .thenAccept(model -> {
+                    MainActivity activity = weakActivity.get();
+                    //model에 3d 모델 저장
+                    if (activity != null) {
+                        activity.model = model;
+                    }
+                })
+                .exceptionally(throwable -> {
+                    Toast.makeText(
+                            this, "Unable to load model", Toast.LENGTH_LONG).show();
+                    return null;
+                });
+        //3D 모델 위의 텍스트나 띄울 화면
+        ViewRenderable.builder()
+                .setView(this, R.layout.view_model_title)
+                .build()
+                .thenAccept(viewRenderable -> {
+                    MainActivity activity = weakActivity.get();
+                    if (activity != null) {
+                        activity.viewRenderable = viewRenderable;
+                    }
+                })
+                .exceptionally(throwable -> {
+                    Toast.makeText(this, "Unable to load model", Toast.LENGTH_LONG).show();
+                    return null;
+                });
+    }
+
+    @Override
+    public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+        if (model == null || viewRenderable == null) {
+            Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Create the Anchor.
+        Anchor anchor = hitResult.createAnchor();
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+        // Create the transformable model and add it to the anchor.
+        TransformableNode model = new TransformableNode(arFragment.getTransformationSystem());
+        model.setParent(anchorNode);
+        model.setRenderable(this.model)
+                .animate(true).start();
+        model.select();
+
+        Node titleNode = new Node();
+        titleNode.setParent(model);
+        titleNode.setEnabled(false);
+        titleNode.setLocalPosition(new Vector3(0.0f, 1.0f, 0.0f));
+        titleNode.setRenderable(viewRenderable);
+        titleNode.setEnabled(true);
+    }
+}
